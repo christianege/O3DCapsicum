d3fa9abbf870ce59b6bc388c37e0c28d8a37dd41
Christian Ege <nospam@familie-ege.net>
2011-02-23 20:37:41 +0100

    moved some files

--- Jalapeño/Main.cs
+++ Jalapeño/Main.cs
@@ -25,25 +25,31 @@ namespace jalapeno
             ret = o3d.disconnect();
             ret = o3d.connect(out fwVersion, out deviceType);
 
-            ret = o3d.startLiveImageServer();
-
-            O3DImage[] images = new O3DImage[2];
-            images[0] = new O3DImage(O3DImage.ImageType.AmplitudeImage);
-            images[1] = new O3DImage(O3DImage.ImageType.DistanceImage);
+            if( 0 == ret )
+			{
+				ret = o3d.startLiveImageServer();
+			}
+			if(0 == ret )
+			{
 			
-			O3D.ImagerData imager_settings = new O3D.ImagerData();
-			ret = o3d.getFrontendSettings(ref imager_settings);
-			imager_settings.delayTime = 100;
-			ret = o3d.setFrontendSettings(imager_settings);
-            ret = o3d.getImageData(ref images,true );
+				O3D.ImagerData imager_settings = new O3D.ImagerData();
+				ret = o3d.getFrontendSettings(ref imager_settings);
+				imager_settings.delayTime = 100;
+				ret = o3d.setFrontendSettings(imager_settings);
+			}
+			if( 0 == ret )
+			{
+				O3DImage[] images = new O3DImage[2];
+	            images[0] = new O3DImage(O3DImage.ImageType.AmplitudeImage);
+	            images[1] = new O3DImage(O3DImage.ImageType.DistanceImage);
+				ret = o3d.getImageData(ref images,true );
+	            Bitmap bmp = images[0].toBitmap(O3DImage.BitmapType.GrayScale);
+    	        bmp.Save("mypic_intensity.png",System.Drawing.Imaging.ImageFormat.Png);
+         		bmp = images[1].toBitmap(O3DImage.BitmapType.GrayScale);
+            	bmp.Save("mypic_distance.png",System.Drawing.Imaging.ImageFormat.Png);
+			}
             ret = o3d.stopLiveImageServer();
             ret = o3d.disconnect();
-
-            Bitmap bmp = images[0].toBitmap(O3DImage.BitmapType.GrayScale);
-            bmp.Save("C:\\mypic_intensity.png",System.Drawing.Imaging.ImageFormat.Png);
-            bmp = images[1].toBitmap(O3DImage.BitmapType.GrayScale);
-            bmp.Save("C:\\mypic_grayscale.png",System.Drawing.Imaging.ImageFormat.Png);
-            System.Console.WriteLine("Hello World");
 		}
 	}
 }
